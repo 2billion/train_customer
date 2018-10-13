2b4b562ef548090220b645a48856274f30e47338
lazybone <lgtwboy@gmail.com>
2018-10-13 21:12:20 +0800

    user info

--- app/src/main/java/com/train/train_manager/act/bean/UserBean.java
+++ app/src/main/java/com/train/train_manager/act/bean/UserBean.java
@@ -2,18 +2,56 @@ package com.train.train_manager.act.bean;
 
 public class UserBean {
 
-    public String userId; // String	是		用户ID
-    public String userName; // String	是		用户姓名
-    public String userHeader; // String	否		用户头像
-    public String userDesc; // String	否		描述
-    public String post; // String	否		职位
-    public String createTime; // String	否		创建时间
-    public String updateTime; // String	否		更新时间
-    public String lastLoginTime; // String	否		最近登录时间
-    public String deptId; // Int	是		部门ID
-    public String deptName; // String	否		部门名称
+    //    public String userId; // String	是		用户ID
+    //    public String userName; // String	是		用户姓名
+    //    public String userHeader; // String	否		用户头像
+    //    public String userDesc; // String	否		描述
+    //    public String post; // String	否		职位
+    //    public String createTime; // String	否		创建时间
+    //    public String updateTime; // String	否		更新时间
+    //    public String lastLoginTime; // String	否		最近登录时间
+    //    public String deptId; // Int	是		部门ID
+    //    public String deptName; // String	否		部门名称
+
+
+    public String userName;  // "管理员",
+    public String password;  // "E10ADC3949BA59ABBE56E057F20F883E",
+    public String theme;  // "azure.min.css",
+    public String deptId;  // 2,
+    public String updateTime;  // "2018-10-10 16:20:03",
+    public String userId;  // "admin",
+    public String cusOrgId;  // 2,
+    public String cardId;  // null,
+    public String deptName;  // null,
+    public String post;  // "系统管理员",
+    public String userHeader;  // null,
+    public String userDesc;  // "我是系统符合机会的风格风格和刚刚好风格和是大概广告费非官方发广告非官方发广告",
+    public String createTime;  // null,
+    public String lastLoginTime;  // null
 
 }
+
+
+//{
+//        "data": {
+//        "userName": "管理员",
+//        "password": "E10ADC3949BA59ABBE56E057F20F883E",
+//        "theme": "azure.min.css",
+//        "deptId": 2,
+//        "updateTime": "2018-10-10 16:20:03",
+//        "userId": "admin",
+//        "cusOrgId": 2,
+//        "cardId": null,
+//        "deptName": null,
+//        "post": "系统管理员",
+//        "userHeader": null,
+//        "userDesc": "我是系统符合机会的风格风格和刚刚好风格和是大概广告费非官方发广告非官方发广告",
+//        "createTime": null,
+//        "lastLoginTime": null
+//        },
+//        "msg": "参数不正确！",
+//        "code": 200
+//        }
 
 
 //{
